7d8e09b311bbc31ced30767c7823264d6b45a8fb
Techcable <Techcable@techcable.net>
2016-05-31 17:01:10 -0600

    Improve applyPatches.sh
    Get rid of all that crazy parenthesis stuff

--- applyPatches.sh
+++ applyPatches.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-(
 PS1="$"
 basedir="$(cd "$1" && pwd -P)"
 workdir="$basedir/work"
 gpgsign="$(git config commit.gpgsign || echo "false")"
 echo "Rebuilding Forked projects.... "
+
+function enableCommitSigningIfNeeded {
+    if [[ "$gpgsign" == "true" ]]; then
+        echo "Re-enabling GPG Signing"
+        # Yes, this has to be global
+        git config --global commit.gpgsign true
+    fi
+}
 
 function applyPatch {
     what=$1
@@ -23,11 +30,6 @@ function applyPatch {
     fi
     cd "$basedir/$target"
 
-    # Disable GPG signing before AM, slows things down and doesn't play nicely.
-    # There is also zero rational or logical reason to do so for these sub-repo AMs.
-    # Calm down kids, it's re-enabled (if needed) immediately after, pass or fail.
-    git config commit.gpgsign false
-
     echo "Resetting $target to $what_name..."
     git remote rm upstream > /dev/null 2>&1
     git remote add upstream "$basedir/$what" >/dev/null 2>&1
@@ -43,26 +45,23 @@ function applyPatch {
         echo "  Something did not apply cleanly to $target."
         echo "  Please review above details and finish the apply then"
         echo "  save the changes with rebuildPatches.sh"
+        enableCommitSigningIfNeeded
         exit 1
     else
         echo "  Patches applied cleanly to $target"
     fi
 }
 
-function enableCommitSigningIfNeeded {
-    if [[ "$gpgsign" == "true" ]]; then
-        git config commit.gpgsign true
-    fi
-}
+# Disable GPG signing before AM, slows things down and doesn't play nicely.
+# There is also zero rational or logical reason to do so for these sub-repo AMs.
+# Calm down kids, it's re-enabled (if needed) immediately after, pass or fail.
+if [[ "$gpgsign" == "true" ]]; then
+    echo "_Temporarily_ disabling GPG signing"
+    git config --global commit.gpgsign false
+fi
 
-# Apply paper
-cd "$basedir"
-(
-	applyPatch "BungeeCord" Waterfall-Proxy HEAD
-    enableCommitSigningIfNeeded
-) || (
-	echo "Failed to apply WaterfallPatches"
-    enableCommitSigningIfNeeded
-	exit 1
-) || exit 1
-)
+
+# Apply patches
+applyPatch BungeeCord Waterfall-Proxy HEAD
+
+enableCommitSigningIfNeeded
