11d082501ee2aa2d6987f91e777c25a741a2a553
Troy Frew <troy@troy.sh>
2016-06-29 17:34:07 -0500

    add submodule checkout to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-./applyPatches.sh && pushd Travertine-Proxy && mvn clean package && popd
-
+git submodule update --recursive --init && ./applyPatches.sh && pushd Travertine-Proxy && mvn clean package && popd
