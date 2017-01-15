bf36a4c5affb31d8a3b3dbd76cb4c7b3b4c3acdc
Techcable <Techcable@techcable.net>
2017-01-15 10:05:31 -0700

    Fix patch conflicts

---
+++ mergeUpstream.sh
@@ -0,0 +1,15 @@
+
+PS1="$"
+basedir=`pwd`
+
+function update {
+    cd "$basedir/$1"
+    git fetch && git reset --hard origin/master
+    cd "$basedir/$1/.."
+    git add $1
+}
+
+update Waterfall
+
+# Update submodules
+git submodule update --recursive
