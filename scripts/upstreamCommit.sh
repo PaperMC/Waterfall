087493151800d70baf85eff4114f905b4198aac0
Shane Freeder <theboyetronic@gmail.com>
2019-02-20 18:51:36 +0000

    Updated Upstream (BungeeCord)
    Also, pulled in papers upstreamCommit script to provide more consistency and information
    across the project
    
    BungeeCord Changes:
    6d6fbb5e Fix two minor formatting issues
    0d6f3ee3 Make 1.13 command injection normal functionality

---
+++ scripts/upstreamCommit.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+(
+set -e
+PS1="$"
+
+function changelog() {
+    base=$(git ls-tree HEAD $1  | cut -d' ' -f3 | cut -f1)
+    cd $1 && git log --oneline ${base}...HEAD
+}
+bungee=$(changelog BungeeCord)
+
+updated=""
+logsuffix=""
+if [ ! -z "$bungee" ]; then
+    logsuffix="$logsuffix\n\nBungeeCord Changes:\n$bungee"
+    if [ -z "$updated" ]; then updated="BungeeCord"; else updated="$updated/BungeeCord"; fi
+fi
+disclaimer="Upstream has released updates that appears to apply and compile correctly.\nThis update has not been tested by PaperMC and as with ANY update, please do your own testing"
+
+if [ ! -z "$1" ]; then
+    disclaimer="$@"
+fi
+
+log="${UP_LOG_PREFIX}Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
+
+echo -e "$log" | git commit -F -
+
+) || exit 1
