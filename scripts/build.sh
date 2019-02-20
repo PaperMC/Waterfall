339460b7c3735cbf4f882b8c76f98b9da9bb884b
Shane Freeder <theboyetronic@gmail.com>
2019-02-20 19:31:30 +0000

    Misc script fixes
    Also, grab upstreamCommit

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-git submodule update --recursive --init && ./scripts/applyPatches.sh && pushd Travertine-Proxy && mvn clean package && popd
+git submodule update --recursive --init && ./scripts/applyPatches.sh
+if [ "$1" == "--jar" ]; then
+    pushd Travertine-Proxy
+    mvn clean package
+fi
---
+++ scripts/upstreamCommit.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+(
+set -e
+PS1="$"
+
+function changelog() {
+    base=$(git ls-tree HEAD $1  | cut -d' ' -f3 | cut -f1)
+    cd $1 && git log --oneline ${base}...HEAD
+}
+waterfall=$(changelog Waterfall)
+
+updated=""
+logsuffix=""
+if [ ! -z "$waterfall" ]; then
+    logsuffix="$logsuffix\n\nWaterfall Changes:\n$waterfall"
+    if [ -z "$updated" ]; then updated="Waterfall"; else updated="$updated/Waterfall"; fi
+fi
+disclaimer="Upstream has released updates that appears to apply and compile correctly.\nThis update has not been tested by PaperMC and as with ANY update, please do your own testing"
+
+if [ ! -z "$1" ]; then
+    disclaimer="$@"
+fi
+
+log="${UP_LOG_PREFIX}Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
+
+echo -e "$log" | git commit -F -
+
+) || exit 1
