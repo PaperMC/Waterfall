85f0a0c69a5d1ffbf759ae5553a3b09d1f520eda
Shane Freeder <theboyetronic@gmail.com>
2020-11-09 01:35:40 +0000

    Capture some script failure states better
    Ensures that failures of primarily applying patches during builds
    should be passed up, this will need a better look over at some point

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git submodule update --init && ./scripts/applyPatches.sh
+git submodule update --init && ./scripts/applyPatches.sh || exit 1
 
 if [ "$1" == "--jar" ]; then
      mvn clean package
